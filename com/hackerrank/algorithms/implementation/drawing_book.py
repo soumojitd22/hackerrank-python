e6bbedbf79ad5723f707f932fd1567306c3f95fb
Soumojit Dutta <soumojitd22@gmail.com>
2019-08-18 17:44:18 +0530

    drawing-book

--- com/hackerrank/algorithms/implementation/drawing_book.py
+++ com/hackerrank/algorithms/implementation/drawing_book.py
@@ -4,6 +4,9 @@ drawing-book
 
 
 def pageCount(n, p):
+    if p == 1 or p == n:
+        return 0
+
     forward = (p - 1) // 2 + (p - 1) % 2
     backward = (n - p) // 2 + (n - p) % 2 if n % 2 == 0 else (n - 1 - p) // 2 + (n - 1 - p) % 2
     return min(forward, backward)
@@ -12,5 +15,6 @@ def pageCount(n, p):
 if __name__ == '__main__':
     n = int(input())
     p = int(input())
+    print(-1 % 2)
     result = pageCount(n, p)
     print(result)
