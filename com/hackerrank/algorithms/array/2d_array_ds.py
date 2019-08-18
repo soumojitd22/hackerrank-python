045e9b28e356e0edd7687ea8fe3c8d98c24e3b67
Soumojit Dutta <soumojitd22@gmail.com>
2019-08-18 15:28:19 +0530

    Renamed

--- com/hackerrank/algorithms/array/2d_array_ds.py
+++ com/hackerrank/algorithms/array/2d_array_ds.py
@@ -21,11 +21,8 @@ def hourglassSum(arr):
 
 
 if __name__ == '__main__':
-    arr = [[1, 1, 1, 0, 0, 0],
-           [0, 1, 0, 0, 0, 0],
-           [1, 1, 1, 0, 0, 0],
-           [0, 0, 2, 4, 4, 0],
-           [0, 0, 0, 2, 0, 0],
-           [0, 0, 1, 2, 4, 0]]
+    arr = []
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
 
     print(hourglassSum(arr))
