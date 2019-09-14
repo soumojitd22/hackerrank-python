b66b9c3720a5017cbd279fd17ff68631bd7ecbd1
Soumojit Dutta <soumojitd22@gmail.com>
2019-09-14 22:31:49 +0530

    Code format

--- com/hackerrank/algorithms/implementation/flatland_space_stations.py
+++ com/hackerrank/algorithms/implementation/flatland_space_stations.py
@@ -5,10 +5,10 @@ flatland-space-stations
 
 def flatland_space_stations(n, c):
     if len(c) == 1:
-        return max(c[0] - 0, n - 1 - c[0])
+        return max(c[0], n - 1 - c[0])
 
     c.sort()
-    return max(max([(c[i] - c[i - 1]) // 2 for i in range(1, len(c))]), c[0] - 0, n - 1 - c[-1])
+    return max(max([(c[i] - c[i - 1]) // 2 for i in range(1, len(c))]), c[0], n - 1 - c[-1])
 
 
 if __name__ == '__main__':
